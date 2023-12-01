5755fa5889c311e1c5d1fb318ef5336e565c676a
Mohammed-Medhat <mamadomedhat10@gmail.com>
2023-12-01 22:37:50 +0200

    Memebershipplane first try

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,12 @@
+import java.time.LocalDate;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        LocalDate s = LocalDate.of(2023,5,5);
+        LocalDate e=LocalDate.of(2024,7,7);
+        MembershipPlan m=new MembershipPlan(s,e);
+        System.out.println("Remaining days: " + m.calculateDaysRemaining(e));
     }
 }
