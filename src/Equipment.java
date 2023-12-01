d8f4abc68bcb8b11672889b65817430424798f33
Youssof Othman <112422200+3ossjunior@users.noreply.github.com>
2023-12-02 00:40:29 +0200

    Update Equipment.java
    lsa

--- src/Equipment.java
+++ src/Equipment.java
@@ -1,2 +1,15 @@
+
+import java.util.ArrayList;
+import java.util.List;
 public class Equipment {
+    public String name[];
+    public int quantity[];
+    public Equipment(String name[] , int quantity[]){
+        this.name=name;
+        this.quantity=quantity;
+
+    }
+
+
+
 }
