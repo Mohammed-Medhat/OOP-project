72e8826e354bc38b6da28f266b53b6551d7cf5b4
AhmedOsama21 <Ahmeddd2112osama@gmail.com>
2023-12-02 13:38:17 +0200

    test

--- src/Customer.java
+++ src/Customer.java
@@ -1,2 +1,14 @@
-public class Customer extends Person {
+public class Customer extends Person
+{
+    public String subscription;
+
+    public Customer(int id, String name, char gender, String address, int phoneNumber, String email,String subscription)
+    {
+        super( id,  name,  gender,  address,  phoneNumber,  email);
+        this.subscription=subscription;
+    }
+    public void displayDetails()
+    {
+        System.out.println("Subscription: " + subscription);
+    }
 }
