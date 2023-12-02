f6c71ad40d0cbfe6b1e338d286341763e4b6d3fb
Abdelrahman22w <abdelrahmanwaheed22w@gmail.com>
2023-12-02 12:29:41 +0200

    person class

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,49 @@
 abstract public class Person {
+    private int id;
+    public String name;
+    public char gender;
+    private String address;
+    private int phoneNumber;
+    private String email;
+
+
+    public Person(int id, String name, char gender, String address, int phoneNumber, String email)
+    {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
