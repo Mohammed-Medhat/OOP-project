88992dda777799105c4859cce820cac75209eafa
AhmedOsama21 <Ahmeddd2112osama@gmail.com>
2023-12-02 13:09:38 +0200

    test

--- src/Coach.java
+++ src/Coach.java
@@ -1,2 +1,16 @@
-public class Coach extends Person {
+public class Coach extends Person
+{
+     public  int workingHours;
+
+     public Coach(int id, String name, char gender, String address, int phoneNumber, String email,int workingHours)
+     {
+         super( id,  name,  gender,  address,  phoneNumber,  email);
+         this.workingHours=workingHours;
+     }
+
+    @Override
+    public void displayDetails()
+    {
+        System.out.println("Working Hours: " + workingHours);
+    }
 }
