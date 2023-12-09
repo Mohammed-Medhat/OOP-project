57fbd7e37792e6b6bdac6840e2b9006df669c469
Mohamed Ali <115904269+mohamedali382@users.noreply.github.com>
2023-12-10 00:45:58 +0200

    Update MembershipPlan.java

--- src/MembershipPlan.java
+++ src/MembershipPlan.java
@@ -1,20 +1,33 @@
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 public class MembershipPlan  {
-    LocalDate startDate;
-    LocalDate endDate;
-    String MonthlyPlan;
-    int no_of_months_registered;
-    float price_of_the_membership_plan;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String MonthlyPlan;
+    private int no_of_months_registered;
+    private float price_of_the_membership_plan;
 
-    public MembershipPlan(LocalDate s,LocalDate e ) {
-        startDate=s;
-        endDate=e;
-
+    public MembershipPlan(LocalDate startDate, LocalDate endDate, String MonthlyPlan, int no_of_months_registered, float price_of_the_membership_plan) {
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.MonthlyPlan = MonthlyPlan;
+        this.no_of_months_registered = no_of_months_registered;
+        this.price_of_the_membership_plan = price_of_the_membership_plan;
     }
-    public int getStartDate()
+    public void LocalDate setStartDate(LocalDate start)
     {
-        return s;
+        startDate = start;
+    }
+    
+    public LocalDate getStartDate()
+    {
+        return startDate;
+    }
+     public void LocalDate setEndDate(LocalDate end){
+         endDate = end;
+     }
+    public LocalDate getEndDate(){
+        return endDate;
     }
 
     public static long calculateDaysRemaining( LocalDate endDate) {
