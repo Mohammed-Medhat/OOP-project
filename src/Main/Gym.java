e5087225099b63f6fe2d67c331796ed76f6cc224
Mohammed-Medhat <mamadomedhat10@gmail.com>
2023-12-12 18:56:04 +0200

    gym

---
+++ src/Main/Gym.java
@@ -0,0 +1,20 @@
+package Main;
+
+import User.Coach;
+import User.Customer;
+import subscription_plan.Subscription;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Gym {
+    private String Team_27;
+    private final String  gname = "Team_27";
+    private final String  gadress = "elabsya";
+    private final String  gphonenum = "01272825488";
+    private List <String> gequipment = new ArrayList<>();
+    public static List <Coach> gcoach = new ArrayList<>();
+    public static List <Subscription> gsub = new ArrayList<>();
+    public static List <Customer> gcus = new ArrayList<>();
+
+}
