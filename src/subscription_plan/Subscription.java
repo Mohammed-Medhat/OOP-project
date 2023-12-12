8105662198835cd1cb4125885226fe798f6804a2
Mohamed Ali <115904269+mohamedali382@users.noreply.github.com>
2023-12-13 01:30:56 +0200

    Update Subscription.java

--- src/subscription_plan/Subscription.java
+++ src/subscription_plan/Subscription.java
@@ -1,13 +1,14 @@
 package subscription_plan;
+import java.time.LocalDate;
+import java.util.Scanner;
 
-import java.io.Serializable;
-
-public class Subscription implements Serializable {
-   private int coach_id;
+public class subcription {
+ private int coach_id;
  private int customer_id;
- MemberShipPlan membership;
+ MembershipPlan membership;
+    Scanner scanner = new Scanner(System.in);
 
- public subcription(int customer_id,int coach_id,MemberShipPlan membership){
+ public subcription(int customer_id,int coach_id,MembershipPlan membership){
      this.customer_id =customer_id;
      this.coach_id = coach_id;
      this.membership = membership;
@@ -20,15 +21,43 @@ public class Subscription implements Serializable {
     public int getCustomer_id() {
         return customer_id;
     }
-   public String toString()
-   {
-      return getCoach_id() + " " + getCustomer_id();
-   }
-   
-    public void displaySubcription() {
-        System.out.println("your ID: "+getCustomer_id());
-        System.out.println("your coach ID: "+getCoach_id());
-        System.out.println("subscription_plan.Subscription date"+membership.getStartDate());
-        System.out.println("Remaining subscription days: " + membership.calculateDaysRemaining());
+    public String toString(){
+     return getCustomer_id() + " " + getCoach_id() + " " + membership;
     }
+
+    public void displaySubcription() {
+
+        System.out.println("your ID: " + getCustomer_id());
+        System.out.println("your coach ID: " + getCoach_id());
+
+        if (membership.calculateDaysRemaining() > 0) {
+            System.out.println("your plan: " + membership.getMonthlyPlan());
+            System.out.println("Remaining subscription days: " + membership.calculateDaysRemaining());
+        }
+        else
+            System.out.println("your membership was ended");
+    }
+
+    public void historySubscriptions(){
+
+
+        System.out.println(" ID: " + getCustomer_id());
+        System.out.println("coach ID: " + getCoach_id());
+        System.out.println("registerd: " + membership.getNo_of_months_registered() + "months");
+        System.out.println("start date: " + membership.getStartDate());
+        System.out.println("End date: " + membership.getEndDate());
+
+
+    }
+
+public void checkCustomersInMonth(){
+    System.out.println("enter month:");
+    int month = scanner.nextInt();
+    if (month == membership.getNo_of_months_registered()){
+        System.out.println(getCustomer_id());
+    }
+}
+
+
+
 }
