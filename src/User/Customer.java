5d5ff2460a7dab32b0fa3c3ac0f3f5b5a5a5b494
Abdelrahman22w <abdelrahmanwaheed22w@gmail.com>
2023-12-15 20:58:54 +0200

    For Customer: 1. Get his coach info (Name, Phone number, working hours) 2. Display for all the Gym Equipment. 3. Display the customer the membership’s plan details. 4. Display the in-body information at a specific date. 5. Display for the user how many kilos need to be reduced according to his body (you can get any calculation through the internet)

--- src/User/Customer.java
+++ src/User/Customer.java
@@ -1,203 +1,206 @@
+package User;
+import System.Equipments;
+import Subscription_plan.subcription;
+import Subscription_plan.InBody;
+import Subscription_plan.MembershipPlan;
+
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
-
+import java.util.List;
 
 public class Customer extends Person {
     private int cusID;
-    private Subscription subscription;
+    private subcription subscription;
     private ArrayList<InBody> inBodies;
     private Coach coach;
     private ArrayList<Equipments> gymEquipments;
     private MembershipPlan membership;
     private static final double TARGET_BMI = 25.0;
 
+        public Customer(int n,String name, char gender, String address, String phoneNumber, String email,String password) {
 
-    public Customer(){
+            super( name, gender, address, phoneNumber, email,password);
 
-    }
-    public Customer(int n,String name, char gender, String address, String phoneNumber, String email,String password) {
-
-        super( name, gender, address, phoneNumber, email,password);
-
-        this.subscription = null;
-        this.inBodies = new ArrayList<>();
-        this.gymEquipments = new ArrayList<>();
-        this.coach = null;
-        this.membership = null;
-        setCusID(n);
-    }
-
-    public String toString() {
-        return cusID + " " + gender + " " + getAddress() + " " + getPhoneNumber() + " " + getEmail();
-    }
-
-    public MembershipPlan getMembership() {
-        return membership;
-    }
-
-    public void setMembership(MembershipPlan membership) {
-        this.membership = membership;
-    }
-
-    public void setCusID(int n){
-        this.cusID=n+1;
-
-}
-public int getCusId()
-{
-    return cusID;
-}
-
-
-
-    public Subscription getSubscription() {
-        return subscription;
-    }
-
-    public void setSubscription(Subscription subscription) {
-        this.subscription = subscription;
-    }
-
-    public ArrayList<InBody> getInBodies() {
-        return inBodies;
-    }
-
-    public void addInBody(InBody inBody) {
-        inBodies.add(inBody);
-    }
-
-    public Coach getCoach() {
-        return coach;
-    }
-    public boolean hasCoach(){
-        return this.coach != null;
-    }
-
-    public void setCoach(Coach coach) {
-        this.coach = coach;
-        coach.numberofTrainee++;
-    }
-
-    public ArrayList<Equipments> getGymEquipments() {
-        return gymEquipments;
-    }
-
-    public void addGymEquipment(Equipments equipment) {
-        gymEquipments.add(equipment);
-    }
-
-    //  Get coach info for the costumer (Name, Phone number, working hours)
-    public void getCoachInfo(Customer customer) {
-        try {
-            Coach customerCoach = customer.getCoach();
-
-            if (customerCoach != null) {
-                System.out.println("Coach Info for Customer " + customer.name + ":");
-                customerCoach.displayBasicInfo();
-                customerCoach.displayDetails();
-            } else {
-                System.out.println("No assigned coach for Customer " + customer.name + ".");
-            }
-        } catch (Exception e) {
-            System.out.println("Error getting coach info: " + e.getMessage());
+            this.subscription = null;
+            this.inBodies = new ArrayList<>();
+            this.gymEquipments = new ArrayList<>();
+            this.coach = null;
+            this.membership = null;
+            setCusID(n);
         }
-    }
 
-
-    //  Display all Gym Equipment
-    public void displayGymEquipmentList() {
-        try {
-            System.out.println("List of Gym Equipment:");
-            for (Equipments equipment : gymEquipments) {
-                System.out.println("Name: " + equipment.getName() + ", Quantity: " + equipment.getQuantity());
-            }
-        } catch (Exception e) {
-            System.out.println("Error displaying gym equipment: " + e.getMessage());
+        public String toString() {
+            return cusID + " " + gender + " " + getAddress() + " " + getPhoneNumber() + " " + getEmail();
         }
-    }
 
-    // : Display membership plan details
-    public void displayMembershipPlanDetails() {
-        try {
-            if (subscription != null) {
-                System.out.println("Membership Plan Information:");
-                System.out.println("Customer ID: " + getCusId());
-                System.out.println("Customer Name: " + name);
-                System.out.println("Subscription date: " + membership.getStartDate());
-                System.out.println("Remaining subscription days: " + MembershipPlan.calculateDaysRemaining());
-                System.out.println("Monthly Plan: " + membership.getMonthlyPlan());
-                System.out.println("Number of months registered: " + membership.getNo_of_months_registered());
-                System.out.println("Price of the membership plan: " + membership.getPrice_of_the_membership_plan());
-            } else {
-                System.out.println("No membership plan information available.");
-            }
-        } catch (Exception e) {
-            System.out.println("Error displaying membership plan details: " + e.getMessage());
+        public MembershipPlan getMembership() {
+            return membership;
         }
+
+        public void setMembership(MembershipPlan membership) {
+            this.membership = membership;
+        }
+
+        public void setCusID(int n){
+            this.cusID=n+1;
+
+    }
+    public int getCusId()
+    {
+        return cusID;
     }
 
-    //  Display in-body information at a specific date
-    public void displayInBodyInformation(Date specificDate) {
-        try {
-            boolean found = false;
 
-            for (InBody inBody : inBodies) {
-                if (inBody.getDateOfInBody().equals(specificDate)) {
-                    System.out.println("InBody Information at " + specificDate + ":");
-                    System.out.println("Date of InBody: " + inBody.getDateOfInBody());
-                    System.out.println("Height: " + inBody.getHeight() + " m");
-                    System.out.println("Total Weight: " + inBody.getTotalWeight() + " kg");
-                    System.out.println("Body Fat Mass: " + inBody.getBodyFatMass() + " kg");
-                    System.out.println("Minerals: " + inBody.getMinerals() + " kg");
-                    System.out.println("Total Body Water: " + inBody.getTotalBodyWater() + " kg");
-                    System.out.println("Protein: " + inBody.getProtein() + " kg");
-                    found = true;
-                    break; // Stop searching once found
+
+        public subcription getSubscription() {
+            return subscription;
+        }
+
+        public void setSubscription(subcription subscription) {
+            this.subscription = subscription;
+        }
+
+        public ArrayList<InBody> getInBodies() {
+            return inBodies;
+        }
+
+        public void addInBody(InBody inBody) {
+            inBodies.add(inBody);
+        }
+
+        public Coach getCoach() {
+            return coach;
+        }
+        public boolean hasCoach(){
+            return this.coach != null;
+        }
+
+        public void setCoach(Coach coach) {
+            this.coach = coach;
+            coach.numberofTrainee++;
+        }
+
+        public ArrayList<Equipments> getGymEquipments() {
+            return gymEquipments;
+        }
+
+        public void addGymEquipment(Equipments equipment) {
+            gymEquipments.add(equipment);
+        }
+
+        //  Get coach info for the costumer (Name, Phone number, working hours)
+        public void getCoachInfo(Customer customer) {
+            try {
+                Coach customerCoach = customer.getCoach();
+
+                if (customerCoach != null) {
+                    System.out.println("Coach Info for Customer " + customer.name + ":");
+                    customerCoach.displayBasicInfo();
+                    customerCoach.displayDetails();
+                } else {
+                    System.out.println("No assigned coach for Customer " + customer.name + ".");
                 }
+            } catch (Exception e) {
+                System.out.println("Error getting coach info: " + e.getMessage());
             }
+        }
 
-            if (!found) {
-                System.out.println("No InBody information available for the specified date.");
+
+        //  Display all Gym Equipment
+        public void displayGymEquipmentList() {
+            try {
+                System.out.println("List of Gym Equipment:");
+                for (Equipments equipment : gymEquipments) {
+                    System.out.println("Name: " + equipment.getName() + ", Quantity: " + equipment.getQuantity());
+                }
+            } catch (Exception e) {
+                System.out.println("Error displaying gym equipment: " + e.getMessage());
             }
-        } catch (Exception e) {
-            System.out.println("Error displaying in-body information: " + e.getMessage());
+        }
+
+        // : Display membership plan details
+        public void displayMembershipPlanDetails() {
+            try {
+                if (subscription != null && membership != null) {
+                    System.out.println("Membership Plan Information:");
+                    System.out.println("Customer ID: " + getCusId());
+                    System.out.println("Customer Name: " + name);
+                    System.out.println("Subscription date: " + membership.getStartDate());
+                    System.out.println("Remaining subscription days: " + membership.calculateDaysRemaining());
+                    System.out.println("Monthly Plan: " + membership.getMonthlyPlan());
+                    System.out.println("Number of months registered: " + membership.getNo_of_months_registered());
+                    System.out.println("Price of the membership plan: " + membership.getPrice());
+                } else {
+                    System.out.println("No membership plan information available.");
+                }
+            } catch (Exception e) {
+                System.out.println("Error displaying membership plan details: " + e.getMessage());
+            }
+        }
+
+        //  Display in-body information at a specific date
+        public void displayInBodyInformation(Date specificDate) {
+            try {
+                boolean found = false;
+
+                for (InBody inBody : inBodies) {
+                    if (inBody.getDateOfInBody().equals(specificDate)) {
+                        System.out.println("InBody Information at " + specificDate + ":");
+                        System.out.println("Date of InBody: " + inBody.getDateOfInBody());
+                        System.out.println("Height: " + inBody.getHeight() + " m");
+                        System.out.println("Total Weight: " + inBody.getTotalWeight() + " kg");
+                        System.out.println("Body Fat Mass: " + inBody.getBodyFatMass() + " kg");
+                        System.out.println("Minerals: " + inBody.getMinerals() + " kg");
+                        System.out.println("Total Body Water: " + inBody.getTotalBodyWater() + " kg");
+                        System.out.println("Protein: " + inBody.getProtein() + " kg");
+                        found = true;
+                        break; // Stop searching once found
+                    }
+                }
+
+                if (!found) {
+                    System.out.println("No InBody information available for the specified date.");
+                }
+            } catch (Exception e) {
+                System.out.println("Error displaying in-body information: " + e.getMessage());
+            }
+        }
+
+        //  Function: Calculate BMI
+        public double calculateBMI(InBody inBody) {
+            return inBody.getTotalWeight() / (inBody.getHeight() * inBody.getHeight());
+        }
+
+
+        //  Display how many kilos need to be reduced according to his body
+        public void displayWeightToReduce() {
+            try {
+                if (!inBodies.isEmpty()) {
+                    InBody latestInBody = inBodies.get(inBodies.size() - 1);
+
+                    double currentBMI = calculateBMI(latestInBody);
+                    double targetWeight = TARGET_BMI * (latestInBody.getHeight() * latestInBody.getHeight());
+
+                    double weightToReduce = latestInBody.getTotalWeight() - targetWeight;
+
+                    System.out.println("Current BMI: " + currentBMI);
+                    System.out.println("Weight to reduce to achieve BMI of " + TARGET_BMI + ": " + weightToReduce + " kg");
+                } else {
+                    System.out.println("No InBody information available to calculate weight reduction.");
+                }
+            } catch (Exception e) {
+                System.out.println("Error calculating weight reduction: " + e.getMessage());
+            }
+        }
+
+        // Override the abstract method in Person class
+        @Override
+        void displayDetails() {
+            System.out.println("Customer Details:");
+            System.out.println("Email: " + getEmail());
+            System.out.println("Address: " + getAddress());
+            System.out.println("Gender: " + gender);
+
         }
     }
-
-    //  Function: Calculate BMI
-    public double calculateBMI(InBody inBody) {
-        return inBody.getTotalWeight() / (inBody.getHeight() * inBody.getHeight());
-    }
-
-
-    //  Display how many kilos need to be reduced according to his body
-    public void displayWeightToReduce() {
-        try {
-            if (!inBodies.isEmpty()) {
-                InBody latestInBody = inBodies.get(inBodies.size() - 1);
-
-                double currentBMI = calculateBMI(latestInBody);
-                double targetWeight = TARGET_BMI * (latestInBody.getHeight() * latestInBody.getHeight());
-
-                double weightToReduce = latestInBody.getTotalWeight() - targetWeight;
-
-                System.out.println("Current BMI: " + currentBMI);
-                System.out.println("Weight to reduce to achieve BMI of " + TARGET_BMI + ": " + weightToReduce + " kg");
-            } else {
-                System.out.println("No InBody information available to calculate weight reduction.");
-            }
-        } catch (Exception e) {
-            System.out.println("Error calculating weight reduction: " + e.getMessage());
-        }
-    }
-
-    // Override the abstract method in Person class
-    @Override
-    void displayDetails() {
-        System.out.println("Customer Details:");
-        System.out.println("Email: " + getEmail());
-        System.out.println("Address: " + getAddress());
-        System.out.println("Gender: " + gender);
-
-    }
-}
